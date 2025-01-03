9df8fa48391c7b1e8f803e8de204021f2d0558fe
AntonioGally <antonio.gally@gmail.com>
2025-01-03 16:51:56 -0300

    feat: :sparkles: Adding tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import "./style/global.css"
+
 function App() {
 
   return (
     <>
-      <h1>oi</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!asd
+      </h1>
     </>
   )
 }
